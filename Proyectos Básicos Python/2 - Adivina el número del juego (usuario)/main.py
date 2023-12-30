2eabc1549e1cc25568b9246e9b6df6f5970c52a8
AdrianBR003 <149155965+AdrianBR003@users.noreply.github.com>
2023-12-30 20:26:31 +0100

    Finalizado

--- Proyectos Básicos Python/2 - Adivina el número del juego (usuario)/main.py
+++ Proyectos Básicos Python/2 - Adivina el número del juego (usuario)/main.py
@@ -1,3 +1,21 @@
 """
 Segundo proyecto de Python: Adivina el número del juego (usuario)
 """
+import random
+
+
+def computer_guess(x): 
+    low = 1
+    high = x 
+    feedback = ''
+    while feedback != 'c': 
+        guess = random.randint(low, high)
+        feedback = input(f'Es {guess} más alto que (H), más bajo (L), o es correcto (C)??').lower()
+        if feedback == 'h': 
+            high = guess -1 
+        elif feedback == 'l': 
+            low = guess + 1
+    print(f'El programa encontró el numero correcto {guess}')
+
+    
+computer_guess(10)
